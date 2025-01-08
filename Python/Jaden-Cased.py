c6a029c24bf9aa8d7cb6cd4cdd8da9c2f9c40c57
Benjamin Mapesa <benleomapesa@gmail.com>
2025-01-08 11:48:58 +0300

    Converting quote to Jaden-Cased

---
+++ Python/Jaden-Cased.py
@@ -0,0 +1,28 @@
+'''
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. 
+For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+Your task is to convert strings to how they would be written by Jaden Smith. 
+The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+'''
+
+def to_jaden_case(string):
+    # Split the sentence into words
+    words = string.split() 
+    # Create an empty list to store capitalized words
+    capitalized_words = [] 
+    for word in words:
+        # Capitalize each word and add it to the list
+        capitalized_words.append(word.capitalize())
+        # Join the words back into a sentence
+        capitalized_sentence = ' '.join(capitalized_words)  
+
+    return capitalized_sentence
+
+    
+
+print(to_jaden_case( "How can mirrors be real if our eyes aren't real"))
